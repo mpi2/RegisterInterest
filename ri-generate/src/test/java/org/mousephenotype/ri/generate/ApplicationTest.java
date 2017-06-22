73b6bab28be854e65e2654ba61ebd227c9bfa47c
mrelac <mrelac@ebi.ac.uk>
2017-06-22 17:09:30 +0100

    Commented out more unneeded dependencies.

---
+++ ri-generate/src/test/java/org/mousephenotype/ri/generate/ApplicationTest.java
@@ -0,0 +1,17 @@
+package org.mousephenotype.ri.generate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by mrelac on 21/06/2017.
+ */
+class ApplicationTest {
+
+    @Test
+    void testCreateWelcome() {
+        fail("Awww!");
+    }
+
+}
