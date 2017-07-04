e0e58c8d2fcc315cf089d0133e4964fb46e40df3
mrelac <mrelac@ebi.ac.uk>
2017-07-04 14:56:51 +0100

    Rolled ExtractGeneConfigBeans into AppConfig. Refactored Application to use the preferred constructor injection rather than Autowiring.

--- ri-extract/src/main/java/org/mousephenotype/ri/extract/Application.java
+++ ri-extract/src/main/java/org/mousephenotype/ri/extract/Application.java
@@ -1,5 +1,7 @@
 package org.mousephenotype.ri.extract;
 
+import org.mousephenotype.ri.core.SqlUtils;
+import org.mousephenotype.ri.core.exceptions.InterestException;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
@@ -7,28 +9,19 @@ import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import javax.annotation.Resource;
+import javax.inject.Inject;
 import javax.sql.DataSource;
-import org.mousephenotype.ri.core.exceptions.InterestException;
-import org.mousephenotype.ri.core.SqlUtils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -41,10 +34,22 @@ import java.util.concurrent.Executors;
  * Created by mrelac on 22/05/2017.
  *
  * This class is intended to be a command-line callable java main program that loads the ri database with the lastest
- * iMits Gene GeneStatus Change Report.
+ * iMits data.
  */
 @EnableBatchProcessing
+@ComponentScan({"org.mousephenotype.ri.extract"})
 public class Application implements CommandLineRunner {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private JobRepository jobRepository;
+    private List<Downloader> downloader;
+    private GeneLoader imitsLoader;
+    private DataSource riDataSource;
+    private SqlUtils sqlUtils;
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(Application.class);
@@ -54,31 +59,26 @@ public class Application implements CommandLineRunner {
         app.run(args);
     }
 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    @JobScope
-    public StepBuilderFactory stepBuilderFactory;
+    @Inject
+    public Application(
+            JobBuilderFactory jobBuilderFactory,
+            StepBuilderFactory stepBuilderFactory,
+            JobRepository jobRepository,
+            List<Downloader> downloader,
+            GeneLoader imitsLoader,
+            DataSource riDataSource,
+            SqlUtils sqlUtils
+    ) {
 
-    @Autowired
-    public JobRepository jobRepository;
-
-    @Resource
-    public List<Downloader> downloader;
-
-    @Autowired
-    public GeneLoader imitsLoader;
-
-    @Autowired
-    public NamedParameterJdbcTemplate jdbc;
-
-    @Autowired
-    @Qualifier("riDataSource")
-    private DataSource riDataSource;
-
-    private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
-    private SqlUtils sqlUtils = new SqlUtils(jdbc);
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.jobRepository = jobRepository;
+        this.downloader = downloader;
+        this.imitsLoader = imitsLoader;
+        this.riDataSource = riDataSource;
+        this.sqlUtils = sqlUtils;
+    }
 
 
 
--- ri-extract/src/main/java/org/mousephenotype/ri/extract/config/AppConfig.java
+++ ri-extract/src/main/java/org/mousephenotype/ri/extract/config/AppConfig.java
@@ -1,6 +1,15 @@
 package org.mousephenotype.ri.extract.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.mousephenotype.ri.core.SqlUtils;
+import org.mousephenotype.ri.core.UrlUtils;
+import org.mousephenotype.ri.core.entities.Gene;
+import org.mousephenotype.ri.core.entities.ImitsStatus;
+import org.mousephenotype.ri.core.exceptions.InterestException;
+import org.mousephenotype.ri.extract.Downloader;
+import org.mousephenotype.ri.extract.GeneLoader;
+import org.mousephenotype.ri.extract.GeneProcessor;
+import org.mousephenotype.ri.extract.GeneWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,15 +24,16 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import org.mousephenotype.ri.core.SqlUtils;
-
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,7 +41,6 @@ import java.util.Map;
  */
 @Configuration
 @PropertySource(value="file:${user.home}/configfiles/${profile}/ri.application.properties")
-@ComponentScan({"org.mousephenotype"})
 @EnableAutoConfiguration(exclude = {
         JndiConnectionFactoryAutoConfiguration.class,
         DataSourceAutoConfiguration.class,
@@ -46,6 +55,42 @@ public class AppConfig {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @NotNull
+    @Value("${GeneStatusChangeUrl}")
+    protected String geneStatusChangeUrl;
+
+    @NotNull
+    @Value("${download.workspace}")
+    protected String downloadWorkspace;
+
+    private enum DownloadFileEnum {
+        GENE_STATUS_CHANGE
+    }
+
+    public DownloadFilename[] filenames;
+
+
+    @PostConstruct
+    public void initialise() {
+        filenames = new DownloadFilename[] {
+                new DownloadFilename(DownloadFileEnum.GENE_STATUS_CHANGE, geneStatusChangeUrl, downloadWorkspace + "/GeneStatusChange.tsv")
+        };
+    }
+
+
+    public class DownloadFilename {
+        public final DownloadFileEnum downloadFileEnum;
+        public String sourceUrl;
+        public final String targetFilename;
+
+        public DownloadFilename(DownloadFileEnum downloadFileEnum, String sourceUrl, String targetFilename) {
+            this.downloadFileEnum = downloadFileEnum;
+            this.sourceUrl = sourceUrl;
+            this.targetFilename = targetFilename;
+        }
+    }
+
+
     @Bean
     public NamedParameterJdbcTemplate jdbc() {
         return new NamedParameterJdbcTemplate(riDataSource());
@@ -56,12 +101,15 @@ public class AppConfig {
         return new SqlUtils(jdbc());
     }
 
+    @NotNull
     @Value("${datasource.ri.url}")
     String riUrl;
 
+    @NotNull
     @Value("${datasource.ri.username}")
     String username;
 
+    @NotNull
     @Value("${datasource.ri.password}")
     String password;
 
@@ -76,8 +124,6 @@ public class AppConfig {
                 .type(BasicDataSource.class)
                 .driverClassName("com.mysql.jdbc.Driver").build();
         ((BasicDataSource) ds).setInitialSize(4);
-
-
         ((BasicDataSource) ds).setLogAbandoned(false);
         ((BasicDataSource) ds).setRemoveAbandoned(false);
 
@@ -85,18 +131,55 @@ public class AppConfig {
 
             logger.info("Using database {} with initial pool size {}", ds.getConnection().getCatalog(), ((BasicDataSource) ds).getInitialSize());
 
-        } catch (Exception e) { }
+        } catch (Exception e) {
+
+        }
 
         return ds;
     }
 
-//    @Bean(name = "sessionFactory")
-//    public SessionFactory getSessionFactory() {
-//
-//        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(riDataSource());
-//        sessionBuilder.scanPackages("org.mousephenotype.cda.db.entity");
-//        sessionBuilder.scanPackages("org.mousephenotype.cda.db.pojo");
-//
-//        return sessionBuilder.buildSessionFactory();
-//    }
+    @Bean
+    public List<Downloader> downloader() {
+        List<Downloader> downloaderList = new ArrayList<>();
+
+        for (DownloadFilename download : filenames) {
+            downloaderList.add(new Downloader(download.sourceUrl, download.targetFilename));
+        }
+
+        return downloaderList;
+    }
+
+
+    // LOADERS, PROCESSORS, AND WRITERS
+
+
+    @Bean(name = "geneLoader")
+    public GeneLoader geneLoader() throws InterestException {
+        Map<GeneLoader.FilenameKeys, String> filenameKeys = new HashMap<>();
+        Map<DownloadFileEnum, DownloadFilename> downloadFilenameMap = new HashMap<>();
+
+        for (DownloadFilename downloadFilename : filenames) {
+
+            downloadFilename.sourceUrl = UrlUtils.getRedirectedUrl(downloadFilename.sourceUrl);                         // Resolve any URL redirection.
+            downloadFilenameMap.put(downloadFilename.downloadFileEnum, downloadFilename);
+        }
+
+        filenameKeys.put(GeneLoader.FilenameKeys.EBI_Gene, downloadFilenameMap.get(DownloadFileEnum.GENE_STATUS_CHANGE).targetFilename);
+
+        return new GeneLoader(filenameKeys);
+    }
+
+    @Bean(name = "geneProcessor")
+    public GeneProcessor geneProcessor() throws InterestException {
+
+        Map<String, Gene> genesMap = sqlUtils().getGenes();
+        Map<String, ImitsStatus> imitsStatusMap = sqlUtils().getImitsStatusMap();
+
+        return new GeneProcessor(imitsStatusMap, genesMap);
+    }
+
+    @Bean(name = "geneWriter")
+    public GeneWriter geneWriter() {
+        return new GeneWriter();
+    }
 }
